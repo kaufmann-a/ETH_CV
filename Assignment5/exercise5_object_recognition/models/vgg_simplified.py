0cc3336dd071149177295f3b057f50e2212ac6f3
Andreas Kaufmann <kaufmann-a@hotmail.ch>
2021-12-05 20:09:32 +0100

    Implemented task3

--- Assignment5/exercise5_object_recognition/models/vgg_simplified.py
+++ Assignment5/exercise5_object_recognition/models/vgg_simplified.py
@@ -29,8 +29,26 @@ class Vgg(nn.Module):
         # #     layer2,
         # #     layer3,
         # #     ...)
+        def get_confblock(in_channels, out_channels):
+            conv_block = nn.Sequential(
+            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=(3, 3), padding=1, stride=(1, 1)),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2, 2))
+            )
+            return conv_block
 
-        ...
+        self.conv_block1 = get_confblock(3, 64)
+        self.conv_block2 = get_confblock(64, 128)
+        self.conv_block3 = get_confblock(128, 256)
+        self.conv_block4 = get_confblock(256, 512)
+        self.conv_block5 = get_confblock(512, 512)
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=512, out_features=fc_layer),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            nn.Linear(in_features=self.fc_layer, out_features=self.classes)
+        )
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
@@ -44,9 +62,12 @@ class Vgg(nn.Module):
         :param x: input image batch tensor, [bs, 3, 32, 32]
         :return: score: predicted score for each class (10 classes in total), [bs, 10]
         """
-        score = None
-        # todo
-        ...
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = self.conv_block4(x)
+        x = self.conv_block5(x)
+        score = self.classifier(x)
 
         return score
 
